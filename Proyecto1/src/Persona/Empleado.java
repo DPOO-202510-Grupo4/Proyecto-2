88b5fc5d9d06c586d075eb9be2aa69f010996f72
Szorro48 <santiago.zorro56@gmail.com>
2025-04-04 08:40:50 -0500

    Persistencia Empleado

--- Proyecto1/src/Persona/Empleado.java
+++ Proyecto1/src/Persona/Empleado.java
@@ -1,5 +1,9 @@
 package Persona;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public abstract class Empleado extends Persona {
 	protected String turno;
 	private static final String NOMBREARCHIVO = "empleados.txt";
@@ -9,7 +13,26 @@ public abstract class Empleado extends Persona {
 	}
 
 
-	
+	public void persistencia(String nombre, Persona persistirPersona){
+
+		crearArchivo(nombre);
+		guardarEmpleado(persistirPersona);
+
+	}
+
+	public void guardarEmpleado(Persona nombreEmpleado){
+
+		
+		try (BufferedWriter empleadoEscrito = new BufferedWriter(new FileWriter(NOMBREARCHIVO, true))){
+			String empleadoFormatoTexto = "Nombre: " + nombre + ", Login: " + login + ", Password: " + password;
+			empleadoEscrito.write(empleadoFormatoTexto);
+			empleadoEscrito.newLine();
+
+		} catch(IOException e){
+			System.err.println("No se puedo guardar el empleado");
+		}
+	}
+
 
 
 
