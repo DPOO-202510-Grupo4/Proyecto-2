340e9078f446e1a20d4743942ca23e3af9d2b610
Szorro48 <santiago.zorro56@gmail.com>
2025-04-04 09:16:30 -0500

    Persistencia Tiquete de Temporada y mas cosas

--- Proyecto1/src/Tiquetes/FastPass.java
+++ Proyecto1/src/Tiquetes/FastPass.java
@@ -5,7 +5,8 @@ import java.util.Date;
 public class FastPass {
 	private Date fecha;
 	private boolean usado;
-	
+	private static final String NOMBREARCHIVO = "tiquetes_fastpass.txt";
+
 	public FastPass(Date fecha, boolean usado) {
 		this.fecha = fecha;
 		this.usado = usado;
--- Proyecto1/src/Tiquetes/TiqueteTemporada.java
+++ Proyecto1/src/Tiquetes/TiqueteTemporada.java
@@ -1,14 +1,59 @@
 package Tiquetes;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 
 public class TiqueteTemporada extends Tiquete {
 	private Date fechaInicio;
 	private Date fechaFinal;
+	private static final String NOMBREARCHIVO = "tiquetes_de_temporada.txt";
 	public TiqueteTemporada(String idTiquete, String categoria, boolean usado, Date fechaInicio, Date fechaFinal) {
 		super(idTiquete, categoria, usado);
 		this.fechaInicio = fechaInicio;
 		this.fechaFinal = fechaFinal;
 	}
 	
+
+
+
+
+
+
+
+
+
+
+
+
+	public void persistencia(String nombre, TiqueteTemporada persistirTiquete){
+
+		crearArchivo(nombre);
+		guardarTiquete(persistirTiquete);
+	}
+
+	public void guardarTiquete(TiqueteTemporada nombreTiqueteTemporada){
+
+		
+		try (BufferedWriter tiqueteEscrito = new BufferedWriter(new FileWriter(NOMBREARCHIVO, true))){
+			String tiqueteFormatoTexto = idTiquete + "," + categoria + ", " + usado + ", " + fechaInicio + ", " + fechaFinal;
+			tiqueteEscrito.write(tiqueteFormatoTexto);
+			tiqueteEscrito.newLine();
+
+		} catch(IOException e){
+			System.err.println("No se pudo guardar el tiquete de temporada");
+		}
+	}	
+}
+
+
+
+
+
+
+
+
+
+
 }
