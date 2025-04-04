ab6f279760aa92778be3b3eaa0c80a07b4ff8308
dcantorni <d.cantorb@uniandes.edu.co>
2025-04-03 21:51:11 -0500

    Creacion clase espectaculos

---
+++ Proyecto1/src/Atracciones/Espectaculos.java
@@ -0,0 +1,11 @@
+package Atracciones;
+import java.util.Date;
+public class Espectaculos {
+    private Date horario;
+    private Date fecha;
+    public Espectaculos(Date horario, Date fecha) {
+        this.horario = horario;
+        this.fecha = fecha;
+    }
+
+}
