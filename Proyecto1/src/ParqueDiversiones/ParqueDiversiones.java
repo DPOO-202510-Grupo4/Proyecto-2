4f84551440cfcd0130ce90f0120e3cc0793b7735
Szorro48 <santiago.zorro56@gmail.com>
2025-04-11 17:16:43 -0500

    intentando comprorbar persistencia AtraccionCultural

--- Proyecto1/src/ParqueDiversiones/ParqueDiversiones.java
+++ Proyecto1/src/ParqueDiversiones/ParqueDiversiones.java
@@ -1,9 +1,12 @@
 package ParqueDiversiones;
 
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
+import Atracciones.Atracciones.AtraccionCultural;
 //import Atracciones.AtraccionCultural;
 import Atracciones.Atracciones.AtraccionMecanica;
+import Persistencias.PersistenciaAtraccionCultural;
 //import Persistencias.PersistenciaAtraccionCultural;
 import Persistencias.PersistenciaAtraccionMecanica;
 //import Persistencias.PersistenciaEmpleado;
@@ -13,19 +16,21 @@ public class ParqueDiversiones {
 
     public static void main(String[] args) {
 
-
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date fecha = formato.parse("07/04/2025 10:00");
+        Date horario = formato.parse("07/04/2025 11:00");
 
 
 
 
         AtraccionMecanica atraccionMecanica = new AtraccionMecanica(
-            "Montaña Rusa",
+            "Zona Gris",
             10,  
             Arrays.asList("Seguridad"),
             "Alta",
             5,
             10,
-            "Montaña Rusa",
+            "La Momia",
             Arrays.asList("Ninguna"),
             true,
             "Alto",
@@ -38,6 +43,28 @@ public class ParqueDiversiones {
         PersistenciaAtraccionMecanica persistenciaAtraccionMecanica = new PersistenciaAtraccionMecanica();
         persistenciaAtraccionMecanica.persistencia("atracciones_mecanicas.txt", atraccionMecanica);
     }
+
+    AtraccionCultural atraccionCultural = new AtraccionCultural(
+        "Zona Colonial",                  // ubicacion
+        30,                               // cupoMax
+        Arrays.asList("Ninguna"),         // restricciones
+        "Media",                          // exclusividad
+        2,                                // minEmpleados
+        5,                                // edadMin
+        "Museo de Historia",              // nombre
+        Arrays.asList("Lluvia"),          // restriccionClima
+        false,                            // deTemporada
+        fecha,                            // fecha
+        horario                           // horario
+    );
+
+        // Persistir atracción cultural
+        PersistenciaAtraccionCultural persistencia = new PersistenciaAtraccionCultural();
+        persistencia.persistencia("atracciones_culturales.txt", atraccionCultural);
+    }
+
+
+
 
     }
 
