e97f5b39eef3ff76288e942a07b9587343e8b55e
dcantorni <d.cantorb@uniandes.edu.co>
2025-04-12 14:46:02 -0500

    Creacion esqueleto consolaEmpleado

--- Proyecto1/src/Presentacion/consolaEmpleado.java
+++ Proyecto1/src/Presentacion/consolaEmpleado.java
@@ -4,33 +4,66 @@ import java.util.Scanner;
 
 public class consolaEmpleado {
 
-    public static void menuAdministrador(Scanner scanner) {
+    public static void menuEmpleado(Scanner scanner) {
         boolean volver = false;
         while (!volver) {
             System.out.println("\n--- MENÚ EMPLEADO ---");
             System.out.println("1. Consultar Turnos");
-            System.out.println("2. Consultar Capacitaciones");
-            System.out.println("3. Registrar Venta");
-            System.out.println("4. Registrar Tiquete");
+            System.out.println("2. Consultar Tareas");
+            System.out.println("3. Consultar Capacitaciones");
+            System.out.println("4. Registrar Venta");
+            System.out.println("5. Registrar Tiquete");
+            System.out.println("6. Volver al menú principal");
             System.out.print("Seleccione una opción: ");
 
             int opcion = Integer.parseInt(scanner.nextLine());
             switch (opcion) {
                 case 1:
-                    System.out.println("Función para registrar empleado (a implementar)");
+                    consultarTurnos(scanner);
                     break;
                 case 2:
-                    System.out.println("Función para asignar turno (a implementar)");
+                    consultarTareas(scanner);
                     break;
                 case 3:
-                    System.out.println("Función para consultar empleados (a implementar)");
+                    consultarCapacitaciones(scanner);
                     break;
                 case 4:
+                    registrarVenta(scanner);
+                    break;
+                case 5:
+                    registrarTiquete(scanner);
+                    break;
+                case 6:
                     volver = true;
                     break;
                 default:
                     System.out.println("Opción no válida.");
             }
         }
+    }
+
+    private static void consultarTurnos(Scanner scanner) {
+        System.out.println("\n--- CONSULTAR TURNOS ---");
+        
+    }
+
+    private static void consultarTareas(Scanner scanner) {
+        System.out.println("\n--- CONSULTAR TAREAS ---");
+        
+    }
+
+    private static void consultarCapacitaciones(Scanner scanner) {
+        System.out.println("\n--- CONSULTAR CAPACITACIONES ---");
+        
+    }
+
+    private static void registrarVenta(Scanner scanner) {
+        System.out.println("\n--- REGISTRAR VENTA ---");
+        
+    }
+
+    private static void registrarTiquete(Scanner scanner) {
+        System.out.println("\n--- REGISTRAR TIQUETE ---");
+        
     }
 }
