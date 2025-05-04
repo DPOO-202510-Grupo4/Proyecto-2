69661103e44619bba1997fdb48c577fae8316498
Szorro48 <santiago.zorro56@gmail.com>
2025-05-03 19:54:40 -0500

    no se que hice

---
+++ Proyecto1/src/test/GestorAtraccionesTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GestorAtraccionesTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("")
+    }
+
+    @Test
+    void testSample() {
+        System.out.println("Test is running");
+    }
+}
+
+
