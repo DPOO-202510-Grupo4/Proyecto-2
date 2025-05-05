352703a47bd96cc8b14b2e387aba0ecdeaff4024
Szorro48 <santiago.zorro56@gmail.com>
2025-05-04 20:54:16 -0500

    primeros dos test gestorAtracciones

--- Proyecto1/test/GestorAtraccionesTest.java
+++ Proyecto1/test/GestorAtraccionesTest.java
@@ -1,12 +1,46 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
+import Atracciones.AtraccionMecanica;
+import Atracciones.GestorAtracciones;
 
 class GestorAtraccionesTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+
+	private GestorAtracciones gestor;
+
+	@BeforeEach
+	public void inicializar(){
+		gestor = new GestorAtracciones();
 	}
+
+	@Test
+	public void testRegistrarAtraccionMecanica(){
+		AtraccionMecanica atraccionMecanica = mock(AtraccionMecanica.class);
+		gestor.registrarAtraccionMecanica(atraccionMecanica);
+		assertTrue(gestor.getAtraccionesMecanicas().contains(atraccionMecanica));
+
+	}
+
+	@Test
+	public void testAtraccionesMecanicasPorUbicacion(){
+		AtraccionMecanica atraccionMecanica1 = mock(AtraccionMecanica.class);
+		when(atraccionMecanica1.getUbicacion()).thenReturn("Norte");
+		AtraccionMecanica atraccionMecanica2 = mock(AtraccionMecanica.class);
+		when(atraccionMecanica2.getUbicacion()).thenReturn("Sur");
+		gestor.registrarAtraccionMecanica(atraccionMecanica1);
+		gestor.registrarAtraccionMecanica(atraccionMecanica2);
+		List<AtraccionMecanica> resultado = gestor.atraccionesMecanicasPorUbicacion("Sur");
+		assertEquals(1, resultado.size());
+		assertTrue(resultado.contains(atraccionMecanica2));
+
+
+	}
+	
 
 }
